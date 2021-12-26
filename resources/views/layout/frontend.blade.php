5a1884a367fc2effce764af83e38a68c0fd0f841
aushev06 <34713423+aushev06@users.noreply.github.com>
2021-12-27 01:36:05 +0300

    Update frontend.blade.php

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -11,7 +11,7 @@ $weekendEnd    = $settings->getWeekendClose($activeSettings);
 <html>
 
 <head>
-    <title>@yield('title', "Jroo - Доставка еды")</title>
+    <title>@yield('title', "Jroo - Доставка еды")wefwefwef</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
     <link rel="stylesheet" href="{{mix('frontend/css/all.css')}}">
