9859a471456d78d686285d7a4bce4849789b150c
aushev06 <34713423+aushev06@users.noreply.github.com>
2022-01-12 03:54:51 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,21 +39,21 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('Payment', function () {
 
             $robokassaLogin  = env('DEMO_MRH_LOGIN', 'Panorama06');
-            $robokassaIsTest = env('TEST_ROBOKASSA', false);
+            $robokassaIsTest = env('TEST_ROBOKASSA', true);
 
             if ($robokassaIsTest) {
-                $robokassaPass = env('TEST_MRH_PASSWORD', 't4Z7ZHtmPwcNM76l6ZnK');
-                $robokassaPass2 = env('TEST_MRH_PASSWORD2', 'bX32l26STXuDB7qmSMrK');
+                $robokassaPass = env('TEST_MRH_PASSWORD', 'ehqGN7wXgScP060WNnF6');
+                $robokassaPass2 = env('TEST_MRH_PASSWORD2', 'LszR30M4FSh3giQe5KlU');
             } else {
-                $robokassaPass = env('TEST_MRH_PASSWORD', 't4Z7ZHtmPwcNM76l6ZnK');
-                $robokassaPass2 = env('TEST_MRH_PASSWORD2', 'bX32l26STXuDB7qmSMrK');
+                $robokassaPass = env('TEST_MRH_PASSWORD', 'ehqGN7wXgScP060WNnF6');
+                $robokassaPass2 = env('TEST_MRH_PASSWORD2', 'LszR30M4FSh3giQe5KlU');
             }
 
             return new Payment(
                 $robokassaLogin,
                 $robokassaPass,
                 $robokassaPass2,
-                $robokassaIsTest
+                true
             );
         });
     }
